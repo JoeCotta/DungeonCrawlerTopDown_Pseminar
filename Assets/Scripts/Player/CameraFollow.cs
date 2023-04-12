ac36ab7f14859225c853fe8975fa93c143b6ec97
Drachenfang <cornell.kampa@web.de>
2023-04-12 17:10:41 +0200

    Camera follows player prototype
    Test room gen hat schon alles assinged fals du es ausprobieren willst; hat auch schon spieler und camera;
    du musst einfach nur Camerafollow auf die main cam ziehen und dann in player beim script die transform vom player  reinziehen.

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position ,desiredPosition ,smoothSpeed);
+        transform.position = smoothPosition;
+    }
+}
