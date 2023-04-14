00c5a962fec492213f9389a899141f8dc3d08e33
Drachenfang <cornell.kampa@web.de>
2023-04-14 23:32:09 +0200

    Main Menu
    Delete save doesnt work because there is no save yet and shop hasnt been added yet => first money system
    big download cause text mesh pro added;
    Quit button only works if you build application(finish application)

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame(){
+        SceneManager.LoadScene("test room gen");
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+}
