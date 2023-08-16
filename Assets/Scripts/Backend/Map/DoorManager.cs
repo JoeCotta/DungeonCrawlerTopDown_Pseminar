03ce5ba5fb00745a983c74fe89a2d4982cd10d8f
Drachenfang <cornell.kampa@web.de>
2023-08-16 15:49:14 +0200

    optimize doormanager

--- Assets/Scripts/Backend/Map/DoorManager.cs
+++ Assets/Scripts/Backend/Map/DoorManager.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
-    public bool isVrt;
-    
     void Start()
     {
         //change if door size generall change
-        if (isVrt) gameObject.transform.localScale *= DataBase.size;
-        else gameObject.transform.localScale *= DataBase.size;
-
+        gameObject.transform.localScale *= DataBase.size;
     }
 }
