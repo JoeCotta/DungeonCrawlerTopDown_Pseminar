985e2352cb13d14c4a42f7c1fa647e8b7f288e92
JoeCotta <91967668+JoeCotta@users.noreply.github.com>
2023-12-14 22:03:37 +0100

    added Rarity Effect Particles in UI

--- Assets/Scripts/Backend/UI/SecondweaponSlot.cs
+++ Assets/Scripts/Backend/UI/SecondweaponSlot.cs
@@ -6,16 +6,40 @@ using UnityEngine.UI;
 public class SecondweaponSlot : MonoBehaviour
 {
     public GameObject weaponImage;
+    [SerializeField] private GameObject effect;
     void Update()
     {
         if (GameManager.player == null) return;
         if (GameManager.player.secondWeapon == null)
         {
             weaponImage.SetActive(false);
+            effect.SetActive(false);
             return;
         }
         else weaponImage.SetActive(true);
+        
         weaponImage.GetComponent<Image>().sprite = GameManager.player.GetComponent<Player>().secondWeapon.GetComponent<SpriteRenderer>().sprite;
         weaponImage.GetComponent<Image>().SetNativeSize();
+    
+
+        // Particles
+        int rarity = GameManager.player.secondWeapon.GetComponent<AlternateWS>().rarity;
+        effect.SetActive(true);
+        switch(rarity)
+        {
+            case 1:
+                effect.GetComponent<ParticleSystem>().startColor = Color.cyan;
+                break;
+            case 2:
+                effect.GetComponent<ParticleSystem>().startColor = Color.magenta;
+                break;
+            case 3:
+                effect.GetComponent<ParticleSystem>().startColor = Color.red;
+                break;
+            default:
+                effect.SetActive(false);
+                break;
+            
+        }
     }
 }
--- Assets/Scripts/Backend/UI/WeaponSlot.cs
+++ Assets/Scripts/Backend/UI/WeaponSlot.cs
@@ -6,16 +6,40 @@ using UnityEngine.UI;
 public class WeaponSlot : MonoBehaviour
 {
     public GameObject weaponImage;
+    [SerializeField] private GameObject effect;
     void Update()
     {
         if (GameManager.player == null) return;
         if (GameManager.player.weapon == null)
         {
             weaponImage.SetActive(false);
+            effect.SetActive(false);
             return;
         }
         else weaponImage.SetActive(true);
+        
         weaponImage.GetComponent<Image>().sprite = GameManager.player.GetComponent<Player>().weapon.GetComponent<SpriteRenderer>().sprite;
         weaponImage.GetComponent<Image>().SetNativeSize();
+    
+
+        // Particles
+        int rarity = GameManager.player.weapon.GetComponent<AlternateWS>().rarity;
+        effect.SetActive(true);
+        switch(rarity)
+        {
+            case 1:
+                effect.GetComponent<ParticleSystem>().startColor = Color.cyan;
+                break;
+            case 2:
+                effect.GetComponent<ParticleSystem>().startColor = Color.magenta;
+                break;
+            case 3:
+                effect.GetComponent<ParticleSystem>().startColor = Color.red;
+                break;
+            default:
+                effect.SetActive(false);
+                break;
+            
+        }
     }
 }
